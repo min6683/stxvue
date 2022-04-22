e50340688688c3285ebd2a24b14c58aa21ab0e7b
min6683 <dev.sm6683@gmail.com>
2022-04-22 18:03:43 +0900

    vue버전발행

---
+++ src/components/ModalView.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="modal-wrap">
+        <div class="modal">
+            <button @click="closeModal"><i class="fas fa-times"></i></button>
+            <b>이 사이트는 스터디용으로 제작되었으며<br>리소스는 원저작자에게 있습니다.</b>
+            <div class="modal-desc">
+                본 사이트는 Chrome에 최적화 되어있습니다. 
+            </div> 
+        </div>
+    </div>
+</template>
+
+<script>
+    import {
+        getCurrentInstance
+    } from 'vue';
+    export default {
+        emits: ['close-modal'],
+        setup() {
+            const {
+                emit
+            } = getCurrentInstance();
+            const closeModal = () => {
+                emit('close-modal');
+            }
+            return {
+                closeModal
+            }
+        }
+    }
+</script>
+
+<style>
+    .modal-wrap {
+        position: fixed;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.7);
+        z-index: 99999;
+        border: 0;
+
+    }
+
+    .modal {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        display: block;
+        width: 400px;
+        height: 250px;
+        background-color: #fff;
+        border-radius: 20px;
+        text-align: center;
+    }
+    .modal i {
+        position: absolute;
+        left: 50%;
+        top: 0px;
+        transform: translateX(-50%);
+        display: block;
+        font-size: 35px;
+        cursor: pointer;
+        border: 0;
+        
+    }
+    .modal-wrap b {
+        position: relative;
+        display: block;
+        margin-top: 40px;
+        margin-bottom: 20px;
+        text-align: center;
+        color: #000;
+        font-weight: 600;
+    }
+    .modal-desc {
+        position: relative;
+        display: block;
+        text-align: center;
+        font-size: 14px;
+        color: #ccc;
+        font-weight: 400;
+        margin-top: 50px;
+    }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeIndex from '@/page/HomeIndex.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [ 
+        {
+            path: '/',
+            name: 'Home',
+            component: HomeIndex,
+        }, 
+        {
+            path: '/company',
+            name: 'Company',
+            component: HomeIndex
+        }, 
+        {
+            path: '/business',
+            name: 'Bussiness',
+            component: HomeIndex
+        }, 
+        {
+            path: '/cb',
+            name: 'Cb',
+            component: HomeIndex
+        }, 
+        {
+            path: '/ad',
+            name: 'AD',
+            component: HomeIndex
+        }, 
+        {
+            path: '/cs',
+            name: 'CS',
+            component: HomeIndex
+        }, 
+        {
+            path: '/job',
+            name: 'Job',
+            component: HomeIndex
+        } 
+    ]
+});
+
+export default router;
